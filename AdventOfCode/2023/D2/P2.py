aaa8f354798e010e02e94a8968e28d5648f338b0
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-02 00:42:13 -0500

    Update P2.py

--- AdventOfCode/2023/D2/P2.py
+++ AdventOfCode/2023/D2/P2.py
@@ -2,7 +2,25 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
+from time import time
+startTime = time()
+inputF = open("input.txt","r")
 
+gameList = [[j.split() for j in i.split(": ")[1].strip().replace(';', ',').split(", ")] for i in inputF.readlines()]
+changeList = {"red" : 0, "green" : 1, "blue": 2}
+sum = 0
+for i in gameList:
+    maxList = [0, 0, 0]
+    for j in i:
+        if int(j[0]) > maxList[changeList[j[1]]]:
+            maxList[changeList[j[1]]] = int(j[0])
+    sum += maxList[0]*maxList[1]*maxList[2]
+print(sum)
+
+inputF.close()
+endTime = time()
+msTaken = round((endTime-startTime)*1000, 3)
+print("{}ms taken".format(msTaken))
 
 inputF.close()
 endTime = time()
