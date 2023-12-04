caf5cb722d0a6682328ec7c0410fa4a0e2ca6642
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-04 00:20:26 -0500

    Update P2.py

--- AdventOfCode/2023/D4/P2.py
+++ AdventOfCode/2023/D4/P2.py
@@ -2,7 +2,19 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
-
+inputList = [i.strip().split(": ")[1] for i in inputF.readlines()]
+winningNums = [[int(j.strip()) for j in i.split(" | ")[0].split()] for i in inputList]
+haveNums = [[int(j.strip()) for j in i.split(" | ")[1].split()] for i in inputList]
+sum = 0
+cardNumber = [1 for _ in range(205)]
+for i, j in enumerate([_ for _ in range(len(winningNums))]):
+    counter = i+1
+    for k in haveNums[i]:
+        if k in winningNums[j]:
+            cardNumber[counter] += cardNumber[i]
+            counter += 1
+    sum += cardNumber[i]
+print(sum)
 
 inputF.close()
 endTime = time()
