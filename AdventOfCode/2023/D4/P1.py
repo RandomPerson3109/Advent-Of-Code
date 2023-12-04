865f3c22ffc9ff2752e8a92acec0385dcfb26690
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-04 00:20:16 -0500

    Update P1.py

--- AdventOfCode/2023/D4/P1.py
+++ AdventOfCode/2023/D4/P1.py
@@ -2,7 +2,18 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
-
+inputList = [i.strip().split(": ")[1] for i in inputF.readlines()]
+winningNums = [[int(j.strip()) for j in i.split(" | ")[0].split()] for i in inputList]
+haveNums = [[int(j.strip()) for j in i.split(" | ")[1].split()] for i in inputList]
+sum = 0
+for i, j in enumerate([_ for _ in range(len(winningNums))]):
+    matches = -1
+    for k in haveNums[i]:
+        if k in winningNums[j]:
+            matches += 1
+    if matches >= 0:
+        sum += 2**matches
+print(sum)
 
 inputF.close()
 endTime = time()
