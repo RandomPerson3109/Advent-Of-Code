0b20960b86b82e40726c9d2aa66da1687c8b774c
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-05 11:40:57 -0500

    Update P1.py

--- AdventOfCode/2023/D5/P1.py
+++ AdventOfCode/2023/D5/P1.py
@@ -2,10 +2,30 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
-fullList = inputF.readlines()
-seedsList = [int(i) for i in fullList[0].strip().split()[1:]]
-fullList = fullList[2:]
-print(fullList)
+fullList = inputF.read().split("\n\n")
+seedList = [int(i) for i in fullList[0].strip().split()[1:]]
+fullList = [i.split("\n")[1:] for i in fullList[1:]]
+for i in range(len(fullList)):
+	for j in range(len(fullList[i])):
+		fullList[i][j] = [int(k) for k in fullList[i][j].split()]
+
+min = float("inf")
+for i in seedList:
+	value = i
+	for j in fullList:
+		counter = 0
+		while value < j[counter][1] or value >= j[counter][1] + j[counter][2]:
+			counter += 1
+			if counter == len(j)-1:
+				break
+		if value < j[counter][1] or value >= j[counter][1] + j[counter][2]:
+			continue
+		else:
+			value += j[counter][0]-j[counter][1]
+			continue
+	if value < min:
+		min = value
+print(min)
 
 inputF.close()
 endTime = time()
