abaa34216f62a5536e537efee7210bb8ba0e749f
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-05 00:14:01 -0500

    Update P1.py

--- AdventOfCode/2023/D5/P1.py
+++ AdventOfCode/2023/D5/P1.py
@@ -2,7 +2,10 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
-
+fullList = inputF.readlines()
+seedsList = [int(i) for i in fullList[0].strip().split()[1:]]
+fullList = fullList[2:]
+print(fullList)
 
 inputF.close()
 endTime = time()
