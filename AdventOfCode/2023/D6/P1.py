e25050fe774a51baab59c05ca10dca90b216a928
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-06 16:19:31 -0500

    Update P1.py

--- AdventOfCode/2023/D6/P1.py
+++ AdventOfCode/2023/D6/P1.py
@@ -1,8 +1,13 @@
 from time import time
+from math import sqrt, floor, ceil
 startTime = time()
 inputF = open("input.txt","r")
 
-
+inputList = [[int(j) for j in i.strip().split()[1:]] for i in inputF.readlines()]
+product = 1
+for t, d in zip(inputList[0], inputList[1]):
+    product *= floor((t+sqrt(t**2-d*4))/2) - ceil((t-sqrt(t**2-d*4))/2) + 1
+print(product)
 
 inputF.close()
 endTime = time()
