7a93daf608a1ae5816868102d215868de7fdc719
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-06 16:19:42 -0500

    Update P2.py

--- AdventOfCode/2023/D6/P2.py
+++ AdventOfCode/2023/D6/P2.py
@@ -1,8 +1,10 @@
 from time import time
+from math import sqrt, floor, ceil
 startTime = time()
 inputF = open("input.txt","r")
 
-
+inputList = [int(''.join([j for j in i.strip().split()[1:]])) for i in inputF.readlines()]
+print(floor((inputList[0]+sqrt(inputList[0]**2-inputList[1]*4))/2) - ceil((inputList[0]-sqrt(inputList[0]**2-inputList[1]*4))/2) + 1)
 
 inputF.close()
 endTime = time()
