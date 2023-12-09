4a7643baf403e3cd6c8d8be8097c13994fe4cb00
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-09 00:28:59 -0500

    Update P1.py

--- AdventOfCode/2023/D9/P1.py
+++ AdventOfCode/2023/D9/P1.py
@@ -2,7 +2,28 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
+def getNextNum(l):
+    lastNums = [l[-1]]
+    while 1:
+        for i in l:
+            if i == 0:
+                allZero = True
+            else:
+                allZero = False
+                break
+        if allZero:
+            break
+        for i in range(len(l)-1):
+            l[i] = l[i+1]-l[i]
+        l.pop(-1)
+        lastNums.append(l[-1])
+    return sum(lastNums)
 
+inputList = [[int(j) for j in i.split()] for i in inputF.readlines()]
+sumList = []
+for i in inputList:
+    sumList.append(getNextNum(i))
+print(sum(sumList))
 
 inputF.close()
 endTime = time()
