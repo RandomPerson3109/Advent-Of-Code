7359549daef6ed7cb64e15978f89ebe8ded39c32
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-09 00:28:31 -0500

    Update P1.py

--- AdventOfCode/2023/D8/P1.py
+++ AdventOfCode/2023/D8/P1.py
@@ -2,7 +2,19 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
+inputList = inputF.read().split("\n\n")
+inputList[1] = [i.split(" = ") for i in inputList[1].split("\n")]
+string = [0 if i == "L" else 1 for i in inputList[0]]
+graph = {}
+for i in inputList[1]:
+	graph[i[0]] = i[1][1:-1].split(", ")
 
+current = "AAA"
+count = 0
+while current != "ZZZ":
+	current = graph[current][string[count%len(string)]]
+	count += 1
+print(count)
 
 inputF.close()
 endTime = time()
