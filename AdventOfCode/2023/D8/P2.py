4d8188fbeeeb7a7c3f0e33f1c8df94c6412e2537
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-09 00:30:28 -0500

    Update P2.py

--- AdventOfCode/2023/D8/P2.py
+++ AdventOfCode/2023/D8/P2.py
@@ -1,54 +1,30 @@
 from time import time
+from math import lcm
 startTime = time()
 inputF = open("input.txt","r")
 
-def getHierarchy(l):
-	instances = [set() for _ in range(5)]
-	jokers = 0
-	for i in l:
-		if i == 1:
-			jokers += 1
-			continue
-		done = False
-		for j in range(4):
-			if i in instances[j]:
-				instances[j+1].add(i)
-				instances[j].remove(i)
-				done = True
-				break
-		if not done:
-			instances[0].add(i)
-	if len(instances[4]) != 0:
-		return 6
-	elif len(instances[3]) != 0:
-		return 5 + jokers
-	elif len(instances[2]) != 0:
-		if len(instances[1]) != 0:
-			return 4
-		else:
-			return 3 if jokers == 0 else 4 + jokers
-	elif len(instances[1]) == 2:
-		return 2 if jokers == 0 else 4
-	elif len(instances[1]) == 1:
-		return 1 if jokers == 0 else 3 if jokers == 1 else 3 + jokers
-	else:
-		return 0 if jokers == 0 else 1 if jokers == 1 else 3 if jokers == 2 else 2 + jokers if jokers == 3 or jokers == 4 else 6
-
-inputList = [i.strip().split() for i in inputF.readlines()]
-for i in inputList:
-	i[0] = [10 if i == 'T' else 1 if i == 'J' else 11 if i == 'Q' else 12 if i == 'K' else 13 if i == 'A' else int(i) for i in list(i[0])]
-	i[1] = int(i[1])
-hierarchy = [[] for _ in range(7)]
-for i in inputList:
-	hierarchy[getHierarchy(i[0])].append(i)
-hierarchy = list(map(sorted, hierarchy))
-sum = 0
-counter = 0
-for i in hierarchy:
-	for j in i:
-		counter += 1
-		sum += counter*j[1]
-print(sum)
+inputList = inputF.read().split("\n\n")
+inputList[1] = [i.split(" = ") for i in inputList[1].split("\n")]
+string = [0 if i == "L" else 1 for i in inputList[0]]
+graph = {}
+startList = []
+for i in inputList[1]:
+	graph[i[0]] = i[1][1:-1].split(", ")
+	if i[0][-1] == "A":
+		startList.append(i[0])
+lcmList = []
+for i in startList:
+	current = i
+	count = 0
+	while current[-1] != "Z":
+		first = False
+		current = graph[current][string[count%len(string)]]
+		count += 1
+	lcmList.append(count)
+lcmN = 1
+for i in lcmList:
+	lcmN = lcm(lcmN, i)
+print(lcmN)
 
 inputF.close()
 endTime = time()
