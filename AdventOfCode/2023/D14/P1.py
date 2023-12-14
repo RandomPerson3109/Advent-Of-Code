4ed8287858ba4c9bb6313417acb52925254bfeaf
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-14 01:42:03 -0500

    Update P1.py

--- AdventOfCode/2023/D14/P1.py
+++ AdventOfCode/2023/D14/P1.py
@@ -2,7 +2,17 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
-
+inputList = inputF.read().split("\n")
+totalLoad = 0
+for i in range(len(inputList[0])):
+    load = len(inputList)
+    for ji, j in enumerate([inputList[k][i] for k in range(len(inputList))]):
+        if j == "O":
+            totalLoad += load
+            load -= 1
+        elif j == "#":
+            load = len(inputList) - ji - 1
+print(totalLoad)
 
 inputF.close()
 endTime = time()
