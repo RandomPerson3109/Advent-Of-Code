6618a33a27c1a7ac4f7a86b583a992c0a3f4ee48
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-14 01:42:12 -0500

    Update P2.py

--- AdventOfCode/2023/D14/P2.py
+++ AdventOfCode/2023/D14/P2.py
@@ -2,7 +2,69 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
+def moveUp(l):
+    newL = [list(i) for i in l]
+    for i in range(len(newL[0])):
+        placeable = 0
+        for ji, j in enumerate([newL[k][i] for k in range(len(newL))]):
+            if j == "O":
+                newL[ji][i] = "."
+                newL[placeable][i] = "O"
+                placeable += 1
+            elif j == "#":
+                placeable = ji + 1
+    return ["".join(i) for i in newL]
+def moveDown(l):
+    newL = [list(i) for i in l]
+    for i in range(len(newL[0])):
+        placeable = len(newL)-1
+        for ji, j in enumerate(reversed([newL[k][i] for k in range(len(newL))])):
+            if j == "O":
+                newL[len(newL)-ji-1][i] = "."
+                newL[placeable][i] = "O"
+                placeable -= 1
+            elif j == "#":
+                placeable = len(newL) - ji - 2
+    return ["".join(i) for i in newL]
 
+def moveLeft(l):
+    newL = [list(i) for i in l]
+    for i in range(len(newL)):
+        placeable = 0
+        for ji, j in enumerate(newL[i]):
+            if j == "O":
+                newL[i][ji] = "."
+                newL[i][placeable] = "O"
+                placeable += 1
+            elif j == "#":
+                placeable = ji + 1
+    return ["".join(i) for i in newL]
+
+def moveRight(l):
+    newL = [list(i) for i in l]
+    for i in range(len(newL)):
+        placeable = len(newL[i])-1
+        for ji, j in enumerate(reversed(newL[i])):
+            if j == "O":
+                newL[i][len(newL[i])-ji-1] = "."
+                newL[i][placeable] = "O"
+                placeable -= 1
+            elif j == "#":
+                placeable = len(newL[i]) - ji - 2
+    return ["".join(i) for i in newL]
+
+def cycle(l):
+    return moveRight(moveDown(moveLeft(moveUp(l))))
+
+inputList = inputF.read().split("\n")
+totalLoads = []
+while len(totalLoads) < 1000:
+    totalLoad = 0
+    for i in range(len(inputList), 0, -1):
+        totalLoad += list(inputList[len(inputList)-i]).count("O")*i
+    totalLoads.append(totalLoad)
+    print(totalLoad)
+    inputList = cycle(inputList)
 
 inputF.close()
 endTime = time()
