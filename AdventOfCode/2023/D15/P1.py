200413eeca48bca7c2d70b58d9df743adc59257d
RandomPerson3109 <151798117+RandomPerson3109@users.noreply.github.com>
2023-12-23 20:38:39 -0500

    Update P1.py

--- AdventOfCode/2023/D15/P1.py
+++ AdventOfCode/2023/D15/P1.py
@@ -2,7 +2,19 @@ from time import time
 startTime = time()
 inputF = open("input.txt","r")
 
+def HASH(s):
+    hash = 0
+    for i in s:
+        hash += ord(i)
+        hash *= 17
+        hash %= 256
+    return hash
 
+inputList = inputF.read().split(",")
+sum = 0
+for i in inputList:
+    sum += HASH(i)
+print(sum)
 
 inputF.close()
 endTime = time()
